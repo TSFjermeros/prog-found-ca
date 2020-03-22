3adb0aa1105cf9b28ac6d06f4b6c82bd54a617b5
TSFjermeros <tom.stian.fjermeros@gmail.com>
2020-03-22 21:09:23 +0100

    add file

---
+++ level-1.js
@@ -0,0 +1,75 @@
+// question 1
+
+var fruit = "kiwi";
+console.log(fruit);
+
+// question 2
+var car = {
+	name: "Ford",
+	color: "Gray"
+};
+console.log(car);
+// question 3
+
+var outOfStock = true;
+if (outOfStock === true) {
+	console.log("Out of stock");
+} else {
+	console.log("In stock");
+}
+
+// question 4
+
+var number = [1, 2, 3, 4];
+console.log(number);
+
+// question 5
+for (var i = 15; i <= 25; i++) {
+	console.log(i);
+}
+// question 6
+
+// I know this isen right, but I tried. I could not figure this out. hope the otherones are correct.
+
+for (var i = 15; i <= 25; i++) {}
+var number = "20";
+var i = 20;
+if (number === i) {
+	console.log("equal");
+} else {
+	console.log("not equal");
+}
+
+// question 7
+var person = {
+	name: "fjermeros",
+	age: "25",
+	color: "white"
+};
+console.log(person);
+var person = {
+	name: "Skreros",
+	age: "27",
+	color: "black"
+};
+console.log(person);
+// question 8
+function whatIDontLike(giraffes) {
+	console.log(giraffes);
+}
+whatIDontLike("I don't like giraffes");
+// question 9
+function whatILike(theWord) {
+	console.log("I like:" + " " + theWord);
+}
+whatILike("Taco");
+whatILike("Pizza");
+
+// question 10
+
+var emptyArray = [];
+
+function logWord(word) {
+	console.log(word);
+}
+logWord("hello");
