a1f667db4e9553a0bb550247e8f594e594c731ce
TSFjermeros <tom.stian.fjermeros@gmail.com>
2020-03-22 21:41:15 +0100

    added level-2

---
+++ level-2.js
@@ -0,0 +1,18 @@
+// question 1
+
+// I did not manage to solve the questin 6 so I did not manage to solve this either.
+
+// question 1
+function iAmAFunction(iAmAFunction) {
+	console.log(iAmAFunction);
+}
+
+iAmAFunction("I am a function");
+
+var innerFunction;
+
+function outerfunction() {
+	console.log();
+}
+
+outerfunction(innerFunction);
